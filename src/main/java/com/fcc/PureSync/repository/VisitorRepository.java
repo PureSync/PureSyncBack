4e29eedd2d0fa77e6830f5c63cbdf857dfa8d811
Hayoung Yu <nhy8001@gmail.com>
2023-12-22 10:24:21 +0900

    [fix] 수정 vistor 중복삽입
    - visitor 중복 삽입 방지

--- src/main/java/com/fcc/PureSync/repository/VisitorRepository.java
+++ src/main/java/com/fcc/PureSync/repository/VisitorRepository.java
@@ -2,7 +2,9 @@ package com.fcc.PureSync.repository;
 
 import com.fcc.PureSync.entity.Visitor;
 import com.fcc.PureSync.vo.CountInfoNativeVo;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 public interface VisitorRepository extends JpaRepository <Visitor, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Visitor findByVisitorDateAndVisitorIp(String visitorDate, String visitorIp);
 
     @Query(value="WITH RECURSIVE DateRange AS (" +
