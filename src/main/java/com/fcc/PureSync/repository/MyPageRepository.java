8130b2bd3698af468da02cf70ad99f8c448d2c41
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-12-26 23:10:54 +0900

    feat : MyPageRepository.java 삭제 - 잔디심기

--- src/main/java/com/fcc/PureSync/repository/MyPageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fcc.PureSync.repository;//package com.fcc.PureSync.repository;
-//
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface MyPageRepository {
-//}
