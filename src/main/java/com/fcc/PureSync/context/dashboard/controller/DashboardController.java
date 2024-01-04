dd66b3ec2b24f9bc4557520a61472c55a1cc33a1
dev-castle <121291175+seongeon0620@users.noreply.github.com>
2024-01-04 23:59:17 +0900

    [Refactor] 대시보드 API 비동기 처리 추가

--- src/main/java/com/fcc/PureSync/context/dashboard/controller/DashboardController.java
+++ src/main/java/com/fcc/PureSync/context/dashboard/controller/DashboardController.java
@@ -33,4 +33,19 @@ public class DashboardController {
         return dashboardService.getDashboardInfo(customUserDetails.getMemSeq(), baseDate);
     }
 
+    @GetMapping(value = {"/dashboard/dates/{type}/{target}", "/dashboard/dates/{baseDate}/{type}/{target}"})
+    public ResultDto getDashboardDetail(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("type") String type, @PathVariable String target, @PathVariable(required = false) String baseDate) {
+        if (customUserDetails == null) {
+            throw new CustomException(CustomExceptionCode.INVALID_JWT);
+        }
+
+        if (baseDate == null) {
+            LocalDate localDate = LocalDate.now();
+            baseDate = localDate.toString();
+        }
+
+        return dashboardService.getDashboardDetail(customUserDetails.getMemSeq(), type, target, baseDate);
+    }
+
+
 }
--- src/main/java/com/fcc/PureSync/context/dashboard/service/DashboardService.java
+++ src/main/java/com/fcc/PureSync/context/dashboard/service/DashboardService.java
@@ -4,6 +4,7 @@ import com.fcc.PureSync.context.dashboard.vo.DashboardDefaultNativeVo;
 import com.fcc.PureSync.context.dashboard.vo.*;
 import com.fcc.PureSync.common.ResultDto;
 import com.fcc.PureSync.context.exercise.repository.ExerciseRepository;
+import com.fcc.PureSync.repository.SleepRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,7 @@ import java.util.*;
 public class DashboardService {
 
     private final ExerciseRepository exerciseRepository;
+    private final SleepRepository sleepRepository;
 
     @Transactional
     public ResultDto getDashboardInfo(Long memSeq, String date) {
@@ -26,6 +28,29 @@ public class DashboardService {
 
         data.put("chart", chartDataList);
         data.put("default", defaultData);
+
+        return ResultDto.of(HttpStatus.OK.value(), HttpStatus.OK, "성공", data);
+    }
+
+    @Transactional
+    public ResultDto getDashboardDetail(Long memSeq, String type, String target, String date) {
+        HashMap<String, Object> data = new HashMap<>();
+        List<?> statsList = null;
+
+        if (type.equals("exercise") && target.equals("monthly")) {
+            statsList =  exerciseRepository.findLastDaysExerciseStats(memSeq, date, 30);
+
+        } else if (type.equals("exercise") && target.equals("yearly")) {
+            statsList =  exerciseRepository.findLastMonthsExerciseStats(memSeq, date, 12);
+
+        } else if (type.equals("sleep") && target.equals("monthly")) {
+            statsList =  sleepRepository.findLastDaysSleepStats(memSeq, date, 30);
+
+        } else if (type.equals("sleep") && target.equals("yearly")) {
+            statsList =  sleepRepository.findLastMonthsSleepStats(memSeq, date, 12);
+        }
+
+        data.put(type, statsList);
 
         return ResultDto.of(HttpStatus.OK.value(), HttpStatus.OK, "성공", data);
     }
